b3502c81c9eedcc2f4c339912b692339424bc50a
Geethanjali-Allam <113144964+Geethanjali-Allam@users.noreply.github.com>
2024-09-13 10:46:27 -0400

    adding get, put , post and delete mappings

--- demo/src/main/java/com/example/demo/controller/CloudVendorAPIServices.java
+++ demo/src/main/java/com/example/demo/controller/CloudVendorAPIServices.java
@@ -1,19 +1,34 @@
 package com.example.demo.controller;
 import com.example.demo.model.cloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudVendor")
 
 public class CloudVendorAPIServices {
+
+    cloudVendor cloudVendor;
     @GetMapping("/{vendorId}")
     public cloudVendor getCloudVendorDetails(@PathVariable String vendorId) {
 
-        return new cloudVendor("c2","vendor1" ,
-                                "Address1","Phone number1");
+        /*return new cloudVendor("c1","vendor1" ,
+                                "Address1","Phone number1");*/
+        return cloudVendor;
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody cloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor created Successfully";
+    }
+    @PutMapping
+    public String UpdateCloudVendorDetails(@RequestBody cloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud vendor Updated Successfully";
+    }
+    @DeleteMapping("{vendorId}")
+    public String DeleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud vendor Deleted Successfully";
     }
 }
 
